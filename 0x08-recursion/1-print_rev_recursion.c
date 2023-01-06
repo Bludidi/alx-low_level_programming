ddf3824fbb3227bfe1600c034c369b1a8c66de2d
Bludidi <bludidi5@gmail.com>
2023-01-06 08:56:24 -0800

    commit

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+nclude "main.h"
+
+/**
+ * *_print_rev_recursion - prints a string in reverse
+ * *@s: string pointer
+ * * Return: Void
+ * */
+void _print_rev_recursion(char *s)
+{
+		if (*s != '\0')
+				{
+							_print_rev_recursion(s + 1);
+									_putchar(*s);
+										}
+}
+
